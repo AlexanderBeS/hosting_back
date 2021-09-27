453f5482884b541a122fa78d32bf2a5c89d52886
Alexander Bespalov <alexander.bespalov@clockwise.software>
2021-09-27 22:57:48 +0300

    added new comand for deploy

--- config/db-config.js
+++ config/db-config.js
@@ -1,4 +1,15 @@
 module.exports = {
+    deploy: {
+        username: 'root',
+        password: 'rootroot',
+        database: 'hosting',
+        host: 'aa1ghlps48ehecq.ch8gvzg2q3r5.us-east-2.rds.amazonaws.com',
+        dialect: 'mysql',
+        seederStorage: 'sequelize',
+        seederStorageTableName: 'sequelizemeta',
+        operatorsAliases: false,
+        port: 3306,
+    },
     local: {
         username: 'root',
         password: 'rootroot',
