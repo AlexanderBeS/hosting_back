31cbde305a3c57d6c9a740bf0de35b5712c3031d
Alexander Bespalov <alexander.bespalov@clockwise.software>
2021-09-29 14:28:26 +0300

    Fixed sql connection

--- config/config.js
+++ config/config.js
@@ -1,9 +1,12 @@
 const db = require('./db-config.js')
 const mode = process.env.NODE_ENV || 'local'
-    //const mode = process.env.NODE_ENV || 'prod'
 
 const config = {
     local: {
+        db: db[mode],
+        jwt_secret: 'somesupersecretsecret'
+    },
+    prod: {
         db: db[mode],
         jwt_secret: 'somesupersecretsecret'
     }
